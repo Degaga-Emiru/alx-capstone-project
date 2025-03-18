953f0f841ca21ae3273c93aba0b1a1cbbdd75e9c
Degaga Emiru <degagaemiru@gmail.com>
2025-03-18 14:07:57 +0300

    creating the book-library react app and installing the necessary dependency also organize my project folder in well structured manner

---
+++ book-library-app/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-slate-500 font-bold'> Welcome to the my capstone project alx for book library -app tha build by React and tailwind css</h1> 
+    </>
+  )
+}
+
+export default App
